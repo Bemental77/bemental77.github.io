433e4b60c4974711c42c13dfd87cdb6a7aec5e3c
Bemental77 <bementcw@gmail.com>
2022-02-10 20:58:43 -0600

    Continued Color Picker Layout. 2 hours.[#181211877]

--- ColorPicker/colorPickerJS.js
+++ ColorPicker/colorPickerJS.js
@@ -1,5 +1,6 @@
 var colorCanvas = document.getElementById('color_canvas');
-var ColorCtx = colorCanvas .getContext('2d');  // This create a 2D context for the canvas
+var ColorCtx = colorCanvas .getContext('2d');// This create a 2D context for the canvas
+//var canvasSlider = document.getElementById('canvas_slider');
 
 // Create a horizontal gradient -
 var color = '#0000ff'
@@ -10,6 +11,7 @@ ColorCtx .fillStyle = gradientH;
 ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
 
 // Create a Vertical Gradient(white to black)
+/*
 let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
 gradientV.addColorStop(0, 'rgba(0,0,0,0)');
 gradientV.addColorStop(1, '#000');
