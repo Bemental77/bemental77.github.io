05fbb433c792bd59e21b13b3a1b901c56a0fdb94
Bemental77 <bementcw@gmail.com>
2022-02-13 21:45:47 -0600

    Making progress to implement RBG value conversion to pybl.  JS now takes input from user. 3 hours total. [#181211906]

--- ColorPicker/colorPickerJS.js
+++ ColorPicker/colorPickerJS.js
@@ -1,34 +1,16 @@
-var colorCanvas = document.getElementById('color_canvas');
-var ColorCtx = colorCanvas .getContext('2d');// This create a 2D context for the canvas
-//var canvasSlider = document.getElementById('canvas_slider');
+function f() {
+    var rNumber = document.getElementById("rNumber").value;
+    var gNumber = document.getElementById("gNumber").value;
+    var bNumber = document.getElementById("bNumber").value;
 
-// Create a horizontal gradient -
-var color = '#0000ff'
-let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
-gradientH.addColorStop(0, '#fff');
-gradientH.addColorStop(1, color);
-ColorCtx .fillStyle = gradientH;
-ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
-
-// Create a Vertical Gradient(white to black)
-/*
-let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
-gradientV.addColorStop(0, 'rgba(0,0,0,0)');
-gradientV.addColorStop(1, '#000');
-ColorCtx .fillStyle = gradientV;
-ColorCtx .fillRect(0, 0, ColorCtx .canvas.width,
-    ColorCtx .canvas.height);
+    if (rNumber, gNumber, bNumber < 256 && rNumber >=1 && gNumber >=1 && bNumber >= 1) {
+        window.alert(rNumber + " " + gNumber + " " + bNumber);
+    }
+    else {
+        window.alert("Please enter a value that is between 1-255")
+    }
 
 
-//Color picking function only seems to result in black backround
-//Needs further testing, unknown cause, thinking gradientH.addColorStop #fff could be the issue.
-//Will test further
-/* colorCanvas.addEventListener('click',function(event){
-    let x = event.clientX;  // Get X coordinate
-    let y = event.clientY;  // Get Y coordinate
-    pixel = ColorCtx.getImageData(x,y,1,1)['data'];   // Read pixel Color
-    rgb = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
-    document.body.style.background = rgb;    // Set this color to body of the document
-});
-*/
+
+}
 
