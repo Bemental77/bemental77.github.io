0480ebfe8dd344da217c940736a9c1c484afd75c
Bemental77 <bementcw@gmail.com>
2022-02-14 11:46:16 -0600

    RBG to PYBL working. 4 hours total. [#181211906]

--- ColorPicker/colorPickerJS.js
+++ ColorPicker/colorPickerJS.js
@@ -16,33 +16,32 @@ draw();
 
 
 function f() {
-    var rNumber = document.getElementById("rNumber").value;
-    var gNumber = document.getElementById("gNumber").value;
-    var bNumber = document.getElementById("bNumber").value;
+    var rNumber = +document.getElementById("rNumber").value;
+    var gNumber = +document.getElementById("gNumber").value;
+    var bNumber = +document.getElementById("bNumber").value;
+
 
     var rho = 0;
     var gamma = 0;
     var beta = 0;
 
-    //The following code produces a NaN if 0 is entered, though cannot find out why.
-    //The math asked to be used to convert to barycentric always results in a 1 if a value over 0 is entered.
-    //When asking the instructor about this, he confirmed that this is the correct math to use.
-    //This must be incorrect however, because this is useless.
+
     if (rNumber, gNumber, bNumber <= 1 && rNumber >=0 && gNumber >=0 && bNumber >= 0) {
         window.alert("You entered: " + rNumber + ", " + gNumber + ", " + bNumber);
-
-
-
-
 
     }
     else {
         window.alert("Please enter a value that is between 0.0 - 1.0");
     }
 
-    window.alert("The rho value is: " + ((1 / rNumber) * rNumber));
-    window.alert("The gamma value is: " + ((1 / gNumber) * gNumber));
-    window.alert("The beta value is: " + ((1 / bNumber) * bNumber));
+    rho = (1 / (rNumber + gNumber + bNumber) * rNumber);
+    gamma = (1 / (rNumber + gNumber + bNumber) * gNumber);
+    beta = (1 / (rNumber + gNumber + bNumber) * bNumber);
+
+
+    window.alert("The rho value is: " + rho.toFixed(3));
+    window.alert("The gamma value is: " + gamma.toFixed(3));
+    window.alert("The beta value is: " + beta.toFixed(3));
 
 }
 
