50e652b8241d6e0e7ba586141d839aa6cc630061
Bemental77 <bementcw@gmail.com>
2022-02-14 10:15:13 -0600

    working on rgb conversion still.  [#181211906]

--- ColorPicker/colorPickerJS.js
+++ ColorPicker/colorPickerJS.js
@@ -1,19 +1,3 @@
-function f() {
-    var rNumber = document.getElementById("rNumber").value;
-    var gNumber = document.getElementById("gNumber").value;
-    var bNumber = document.getElementById("bNumber").value;
-
-    if (rNumber, gNumber, bNumber < 256 && rNumber >=1 && gNumber >=1 && bNumber >= 1) {
-        window.alert(rNumber + " " + gNumber + " " + bNumber);
-    }
-    else {
-        window.alert("Please enter a value that is between 1-255")
-    }
-
-
-
-}
-
 function draw() {
     var canvas = document.getElementById('color_canvas');
     if (canvas.getContext) {
@@ -28,3 +12,43 @@ function draw() {
     }
 }
 draw();
+
+
+
+function f() {
+    var rNumber = document.getElementById("rNumber").value;
+    var gNumber = document.getElementById("gNumber").value;
+    var bNumber = document.getElementById("bNumber").value;
+
+    var rho = 0;
+    var gamma = 0;
+    var beta = 0;
+
+    //(1/bNumber) * bNumber;
+
+    if (rNumber, gNumber, bNumber <= 1 && rNumber >=0 && gNumber >=0 && bNumber >= 0) {
+        window.alert("You entered: " + rNumber + " " + gNumber + " " + bNumber);
+
+        if (rNumber = 0) {
+            rho = 0
+        }
+        if (gNumber = 0) {
+            gamma = 0
+        }
+        if (bNumber = 0) {
+            beta = 0
+        }
+        window.alert("The rho value is: " + ((1/rNumber) * rNumber);
+        window.alert("The gamma value is: " + gamma);
+        window.alert("The beta value is: " + beta);
+
+
+    }
+    else {
+        window.alert("Please enter a value that is between 0.0 - 1.0")
+    }
+
+
+}
+
+
