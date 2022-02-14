471995983e583bc718b1e60082c16c82f9de5afd
Bemental77 <bementcw@gmail.com>
2022-02-14 10:28:30 -0600

    working on rgb conversion still. 3 hours. IMO un-functional math. [#181211906]

--- ColorPicker/colorPickerJS.js
+++ ColorPicker/colorPickerJS.js
@@ -24,28 +24,30 @@ function f() {
     var gamma = 0;
     var beta = 0;
 
-    //(1/bNumber) * bNumber;
-
+    //The following code produces a NaN if 0 is entered, though cannot find out why.
+    //The math asked to be used to convert to barycentric always results in a 1 if a value over 0 is entered.
+    //When asking the instructor about this, he confirmed that this is the correct math to use.
+    //This must be incorrect however, because this is useless.
     if (rNumber, gNumber, bNumber <= 1 && rNumber >=0 && gNumber >=0 && bNumber >= 0) {
         window.alert("You entered: " + rNumber + " " + gNumber + " " + bNumber);
 
-        if (rNumber = 0) {
-            rho = 0
+        if (rNumber === 0) {
+            rho = 0.0;
         }
-        if (gNumber = 0) {
-            gamma = 0
+        if (gNumber === 0) {
+            gamma = 0;
         }
-        if (bNumber = 0) {
-            beta = 0
+        if (bNumber === 0) {
+            beta = 0;
         }
-        window.alert("The rho value is: " + ((1/rNumber) * rNumber);
-        window.alert("The gamma value is: " + gamma);
-        window.alert("The beta value is: " + beta);
+        window.alert("The rho value is: " + ((1 / rNumber) * rNumber));
+        window.alert("The gamma value is: " + ((1 / gNumber) * gNumber));
+        window.alert("The beta value is: " + ((1 / bNumber) * bNumber));
 
 
     }
     else {
-        window.alert("Please enter a value that is between 0.0 - 1.0")
+        window.alert("Please enter a value that is between 0.0 - 1.0");
     }
 
 
