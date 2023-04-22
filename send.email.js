158a6fa9b2a26a982b156a06aa3dbf701530d2a7
Bemental77 <bementcw@gmail.com>
2023-04-22 13:27:04 -0500

    recompile

--- send.email.js
+++ send.email.js
@@ -1,19 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 function sendEmail() {
     var _a, _b, _c;
     var name = (_a = document.getElementById("name")) === null || _a === void 0 ? void 0 : _a.value;
     var email = (_b = document.getElementById("email")) === null || _b === void 0 ? void 0 : _b.value;
     var message = (_c = document.getElementById("message")) === null || _c === void 0 ? void 0 : _c.value;
     var emailBody = "From: ".concat(name, " <").concat(email, ">\r\nTo: caseybement@caseybement.com\r\nSubject: Message from your website\r\n\r\n").concat(message);
-    var emailAddress = process.env.emailaddress;
-    var password = process.env.password;
     fetch('https://smtp.gmail.com:587', {
         method: 'POST',
         body: emailBody,
         headers: {
             'Content-Type': 'text/plain',
-            'Authorization': "Basic ".concat(btoa("".concat(emailAddress, ":").concat(password))),
+            'Authorization': "Basic ".concat(btoa("".concat(process.env.EMAIL_ADDRESS, ":").concat(process.env.PASSWORD))),
             'X-Requested-With': 'XMLHttpRequest'
         }
     })
