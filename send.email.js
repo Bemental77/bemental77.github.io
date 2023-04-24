156d3beee895ec6e786254720869ac4c1d0f9824
Bemental77 <bementcw@gmail.com>
2023-04-24 18:45:28 -0500

    compile

--- send.email.js
+++ send.email.js
@@ -1,32 +1,76 @@
-"use strict";
-var config_js_1 = require("./config.js");
-function sendEmail() {
-    var _a, _b, _c;
-    var name = (_a = document.getElementById("name")) === null || _a === void 0 ? void 0 : _a.value;
-    var emailaddress = (_b = document.getElementById("email")) === null || _b === void 0 ? void 0 : _b.value;
-    var message = (_c = document.getElementById("message")) === null || _c === void 0 ? void 0 : _c.value;
-    var emailBody = "From: ".concat(name, " <").concat(emailaddress, ">\r\nTo: caseybement@caseybement.com\r\nSubject: Message from your website\r\n\r\n").concat(message);
-    fetch('https://smtp.sendgrid.net:587', {
-        method: 'POST',
-        body: emailBody,
-        headers: {
-            'Content-Type': 'text/plain',
-            'Authorization': "Basic ".concat(btoa("".concat('apikey', ":").concat(window.config.PASSWORD))),
-            'X-Requested-With': 'XMLHttpRequest'
-        }
-    })
-        .then(function (response) {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        alert('Email sent successfully!');
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('message').value = '';
-    })
-        .catch(function (error) {
-        console.error('There was a problem sending the email:', error);
-        alert('There was a problem sending the email. Please try again later.');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-    console.log(config_js_1.config.EMAIL_ADDRESS)
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+// get the form element
+var form = document.querySelector('form');
+if (form) { // check if the form element exists
+    // add event listener to the form on submit
+    form.addEventListener('submit', function (event) { return __awaiter(_this, void 0, void 0, function () {
+        var nameInput, emailInput, messageInput, name, email, message, response, data;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    // prevent default form submission behavior
+                    event.preventDefault();
+                    nameInput = document.querySelector('#name');
+                    emailInput = document.querySelector('#email');
+                    messageInput = document.querySelector('#message');
+                    name = nameInput.value;
+                    email = emailInput.value;
+                    message = messageInput.value;
+                    return [4 /*yield*/, fetch('sendemail/pages/api/send', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                name: name,
+                                email: email,
+                                message: message
+                            })
+                        })];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    console.log(data);
+                    return [2 /*return*/];
+            }
+        });
+    }); });
 }
