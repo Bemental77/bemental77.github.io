5cda7faff3bd646a24614a820266cfc9090411b9
Bemental77 <bementcw@gmail.com>
2023-04-24 20:03:14 -0500

    compile

--- send.email.js
+++ send.email.js
@@ -34,7 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-function sendEmail() {
+function sendEmailClientSide() {
     var _this = this;
     var form = document.querySelector('form[name="theForm"]');
     if (form) {
