b7be5c77305e243306e3c7c555a7366fc206e69a
Bemental77 <bementcw@gmail.com>
2024-09-23 22:30:04 -0500

    g s

--- n64/N64Wasm/dist/romlist.js
+++ n64/N64Wasm/dist/romlist.js
@@ -3,8 +3,8 @@ var ROMLIST = [
   {url:"../roms/banjo-tooie.z64",title:"banjo-tooie"},
   {url:"../roms/gauntlet.z64",title:"Gauntlet"},
   {url:"../roms/oot.z64",title:"LoZ OOT"},
-  {url:"../roms/smash.z64",title:"Smash Bros"},
-  {url:"../roms/simcity.z64",title:"SimCity"}
+  {url:"../roms/starfox.z64",title:"starfox"},
+  {url:"../roms/goldeneye.z64",title:"goldeneye"},
 
 
 ];
