810ff0f964723eaec08eab7572eababb9f9fde3c
Bemental77 <bementcw@gmail.com>
2024-09-23 23:04:02 -0500

    roms

--- n64/N64Wasm/dist/romlist.js
+++ n64/N64Wasm/dist/romlist.js
@@ -3,6 +3,10 @@ var ROMLIST = [
   {url:"../roms/banjo-tooie.z64",title:"banjo-tooie"},
   {url:"../roms/oot.z64",title:"LoZ OOT"},
   {url:"../roms/starfox.z64",title:"starfox"},
-
+  {url:"../roms/nba-hangtime.z64",title:"nba-hangtime"},
+  {url:"../roms/papermario.z64",title:"papermario"},
+  {url:"../roms/pokemonstadium.z64",title:"pokemonstadium"},
+  {url:"../roms/puzzlebobble.z64",title:"puzzlebobble"},
+  {url:"../roms/podracer.z64",title:"podracer"},
 
 ];
