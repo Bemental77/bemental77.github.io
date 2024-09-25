19fffc3b5c37109a0a339a2c4a872aaddf74d210
Bemental77 <bementcw@gmail.com>
2024-09-25 17:20:39 -0500

    crusin

--- n64/N64Wasm/dist/romlist.js
+++ n64/N64Wasm/dist/romlist.js
@@ -3,10 +3,9 @@ var ROMLIST = [
   {url:"../roms/banjo-tooie.z64",title:"banjo-tooie"},
   {url:"../roms/oot.z64",title:"LoZ OOT"},
   {url:"../roms/starfox.z64",title:"starfox"},
-  {url:"../roms/nba-hangtime.z64",title:"nba-hangtime"},
   {url:"../roms/papermario.z64",title:"papermario"},
-  {url:"../roms/pokemonstadium.z64",title:"pokemonstadium"},
   {url:"../roms/puzzlebobble.z64",title:"puzzlebobble"},
   {url:"../roms/podracer.z64",title:"podracer"},
-
+  {url:"../roms/crusin.z64",title:"Crusin USA"},
+  {url:"../roms/fightingforce.z64",title:"Fighting Force"}
 ];
