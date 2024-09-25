2730cb4364e8cd0c54e1dec09568e66264eb07c7
Bemental77 <bementcw@gmail.com>
2024-09-25 17:32:04 -0500

    ok

--- n64/N64Wasm/dist/romlist.js
+++ n64/N64Wasm/dist/romlist.js
@@ -4,8 +4,10 @@ var ROMLIST = [
   {url:"../roms/oot.z64",title:"LoZ OOT"},
   {url:"../roms/starfox.z64",title:"starfox"},
   {url:"../roms/papermario.z64",title:"papermario"},
-  {url:"../roms/puzzlebobble.z64",title:"puzzlebobble"},
   {url:"../roms/podracer.z64",title:"podracer"},
   {url:"../roms/crusin.z64",title:"Crusin USA"},
-  {url:"../roms/fightingforce.z64",title:"Fighting Force"}
+  {url:"../roms/blitz2001.z64",title:"Blitz 2001"},
+  {url:"../roms/pkmnsnap.z64",title:"Pokemon Snap"},
+  {url:"../roms/sm64.z64",title:"SM64"},
+  {url:"../roms/thewheel.z64",title:"The Wheel"}
 ];
